6d2649c10c39e56163b5be1724c27740ef9c95c9
erbec-13 <erbec13@gmail.com>
2025-10-29 15:08:59 -0400

    Add leafblower. Add primitive blowing mechanic to leafblower.

--- Assets/Scripts/LeafBlowerMechanics.cs
+++ Assets/Scripts/LeafBlowerMechanics.cs
@@ -3,12 +3,43 @@ using UnityEngine;
 public class LeafBlowerMechanics : MonoBehaviour
 {
     private Rigidbody rigidBody;
-    
+    public float blowRange = 5f;
+    public float blowRadius = 1f;
+    private string blowableTag = "Blowable";
+    public float blowForce = 10f;
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
     }
 
-    void Update() {
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            Blow();
+        }
+    }
+
+    void Blow() {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, blowRadius, transform.forward, blowRange);
+
+        //Debug.DrawRay(transform.position, transform.forward * blowRange, Color.red, 0.5f);
+
+        foreach (RaycastHit hit in hits)
+        {
+            // Make sure the object has the desired tag
+            if (hit.collider.CompareTag(blowableTag))
+            {
+                Rigidbody rb = hit.collider.attachedRigidbody;
+
+                if (rb != null)
+                {
+                    // Calculate direction and apply force
+                    Vector3 forceDir = hit.collider.transform.position - transform.position;
+                    rb.AddForce(forceDir.normalized * blowForce, ForceMode.Impulse);
+                }
+            }
+        }
     }
 }
